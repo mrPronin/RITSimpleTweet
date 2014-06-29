de822a7dc3f0b0c0c992932ccb33d0741c1408d2
Alexander Pronin <pronin.alx@gmail.com>
2014-06-29 21:33:12 +0300

    Initial Commit

---
+++ RITSimpleTweet/RITViewController.h
@@ -0,0 +1,13 @@
+//
+//  RITViewController.h
+//  RITSimpleTweet
+//
+//  Created by Pronin Alexander on 29.06.14.
+//  Copyright (c) 2014 Pronin Alexander. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RITViewController : UIViewController
+
+@end
